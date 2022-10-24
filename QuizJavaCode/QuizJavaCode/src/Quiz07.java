8b1e18b2d90ad5f966e7a559f48464ed34890819
algokelvin-373 <kelvinht373@gmail.com>
2022-10-25 00:40:43 +0700

    [Java] Quiz 06 - 07

--- QuizJavaCode/QuizJavaCode/src/Quiz07.java
+++ QuizJavaCode/QuizJavaCode/src/Quiz07.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Quiz07 {
+import java.util.Scanner;
+
+public class Quiz07 {
+    public static void main(String[] args) {
+        // Function - Looping - Conditional
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input n: ");
+        int n = input.nextInt();
+        set(n);
+
+    }
+
+    // A. y < n+x || B. y <= n-x
+    // A. y < n+x || B. y < n-x
+    // A. y <= n+x || B. y < n-x
+    // A. y <= n+x || B. y <= n-x
+    private static void set(int n) {
+        for (int x = 1; x <= n; x++) {
+            for (int y = 1; y < n + x; y++) {
+                if (y <= n - x) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
