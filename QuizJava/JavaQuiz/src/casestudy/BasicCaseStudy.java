7b1e4cdb42d0d887e8532e3240d54f2e44a1e006
algokelvin-373 <kelvinht373@gmail.com>
2022-10-29 21:36:10 +0700

    [Java] Repair Project Quiz Java

---
+++ QuizJava/JavaQuiz/src/casestudy/BasicCaseStudy.java
@@ -0,0 +1,7 @@
+package casestudy;
+
+public class BasicCaseStudy {
+    public static void main(String[] args) {
+        System.out.println("Test Case Study");
+    }
+}
--- QuizJavaCode/QuizJavaCode/src/Quiz01.java
+++
@@ -1,8 +0,0 @@
-public class Quiz01 {
-    public static void main(String[] args) {
-        // String Object - "substring"
-
-        String data = "I love coding Java Language";
-        System.out.println(data.substring(14,18));
-    }
-}
