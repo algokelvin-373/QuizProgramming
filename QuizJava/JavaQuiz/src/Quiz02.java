88cc0393b090acbe7e8b09ecaf9940c5623506d5
algokelvin-373 <kelvinht373@gmail.com>
2022-11-02 01:04:02 +0700

    [Java] Fixing Quiz 02

--- QuizJava/JavaQuiz/src/Quiz02.java
+++ QuizJava/JavaQuiz/src/Quiz02.java
@@ -5,11 +5,10 @@ public class Quiz02 {
         // Looping Conditional
         Scanner input =  new Scanner(System.in);
 
-        System.out.print("Input a: ");
+        System.out.print("Input a, b, n: ");
+
         int a = input.nextInt();
-        System.out.print("Input b: ");
         int b = input.nextInt();
-        System.out.print("Input n: ");
         int n = input.nextInt();
 
         int Un = a + b*n;
